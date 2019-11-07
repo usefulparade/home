958e454c04b850f0faa033c8a764dd55e01d3baa
usefulparade <breadclip.cooperative@gmail.com>
2019-11-07 00:38:28 -0500

    switched color APIs

--- tools/placeforaperson/colors.js
+++ tools/placeforaperson/colors.js
@@ -1,42 +1,47 @@
 function colortime(){
-    var url = "http://colormind.io/api/";
+    var base = "https://www.thecolorapi.com";
+    var randomColor = [getRandomColorVal(15, 200), getRandomColorVal(10, 155), getRandomColorVal(100, 255)];
+    var modes = ["monochrome", "monochrome-dark"];
+    var scheme = "/scheme?rgb=rgb(" + randomColor[0] + "," + randomColor[1] + "," + randomColor[2];
+    var mode = ")&mode=" + modes[Math.floor(Math.random() * (modes.length))];
+    var count = "&count=2";
+    var request = new XMLHttpRequest();
+    var url = base + scheme + mode + count;
+    console.log(url);
+    var p;
     var data = {
         model : "default",
-    }
-
-    var c1 = 0;
-    var c2 = 1;
-    var p = [];
-
-    var http = new XMLHttpRequest();
-
-    http.onreadystatechange = function() {
-        if(http.readyState == 4 && http.status == 200) {
-            var palette = JSON.parse(http.responseText).result;
-            p = palette;
-
-            c1 = Math.floor(Math.random()*p.length);
-            c2 = Math.floor(Math.random()*p.length);
-            console.log(p[c1], p[c2]);
-
-            while (Math.abs(p[c1][0] - p[c2][0]) < 30 && Math.abs(p[c1][1] - p[c2][1]) < 30 && Math.abs(p[c1][2] - p[c2][2]) < 30){
-                console.log("oh no!");
-                c2 = Math.floor(Math.random()*p.length);
-                console.log(p[c1], p[c2]);
-            }
-        }
-
-        //STYLE THE PAGE
-        document.getElementById("theBuilding").style.color = "rgba(" + p[c1][0] + "," + p[c1][1] + "," + p[c1][2];
-        document.body.style.backgroundColor = "rgba(" + p[c2][0] + "," + p[c2][1] + "," + p[c2][2];
-
-        //STYLE THE BUTTON
-        document.getElementById("theButtonText").style.color = "rgba(" + p[c1][0] + "," + p[c1][1] + "," + p[c1][2];
-        document.getElementById("theButton").style.backgroundColor = "rgba(" + p[c2][0] + "," + p[c2][1] + "," + p[c2][2];
-        document.getElementById("theButton").style.borderColor = "rgba(" + p[c1][0] + "," + p[c1][1] + "," + p[c1][2];
     };
 
-    //console.log(p);
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data));
+    request.open('GET', url, true);
+    request.send(JSON.stringify(data));
+
+    
+
+    request.onreadystatechange = function(){
+        if (request.readyState == 4 && request.status == 200){
+            var palette = JSON.parse(request.response);
+            p = palette;
+            console.log(p);
+
+
+            // STYLE THE PAGE
+            document.getElementById("theBuilding").style.color = p.colors[0].hex.value;
+            document.body.style.backgroundColor = p.colors[1].hex.value;
+
+            //STYLE THE BUTTON
+            document.getElementById("theButtonText").style.color = p.colors[0].hex.value;
+            document.getElementById("theButton").style.backgroundColor = p.colors[1].hex.value;
+            document.getElementById("theButton").style.borderColor = p.colors[0].hex.value;
+
+        } else {
+        }
+
+        
+        
+    };
+}
+
+function getRandomColorVal(min, max){
+    return Math.floor(Math.random() * (max-min) + min);
 }

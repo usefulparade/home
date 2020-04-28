17a9b0f3f7efc4544573b4e009db9bb67f0dfe48
usefulparade <breadclip.cooperative@gmail.com>
2020-04-28 02:48:18 -0400

    huge update to the GSL. added webmidi, scale selector, oscillators, and more.

--- tools/gameoflifesynth/sketch.js
+++ tools/gameoflifesynth/sketch.js
@@ -16,11 +16,30 @@ var context;
 var touchIsDown;
 var paused;
 var scaleVal, microInterval;
-
 var filter;
 
+var webMidiSupported;
+var clockCount;
+var devices = [];
+var deviceLabel, deviceSelector, outputDevice, midiParent;
+var channelLabel, channelSelector, activeChannel;
+var clockCheckbox, startCheckbox;
+
+
+
+WebMidi.enable(function(err) {
+  if (err) {
+    console.log("An error occurred", err);
+    webMidiSupported = false;
+  } else {
+    webMidiSupported = true;
+    console.log(WebMidi.inputs);
+    console.log(WebMidi.outputs);
+  }
+});
+
 function setup() {
-  
+
   context = getAudioContext();
   context.suspend();
 
@@ -85,6 +104,7 @@ function setup() {
   for (j=0; j<columns; j++){
     for (k=0;k<rows;k++){
       synths[j][k] = new p5.Oscillator();
+      synths[j][k].setType("triangle");
       synths[j][k].freq(midiToFreq(notes[j]) * random(-1,1)*2);
       
       synths[j][k].start();
@@ -119,6 +139,54 @@ function setup() {
   filterSliderChange();
   // init();
 
+  midiParent = document.getElementById('midiOptions');
+
+  
+
+  if (webMidiSupported){
+    var divider = createP("~ ~ ~");
+    divider.parent(midiParent);
+    deviceLabel = createP("midi output:");
+    deviceLabel.parent(midiParent);
+    
+    deviceSelector = createSelect();
+    deviceSelector.parent(midiParent);
+    // channelSelector.addClass("select");
+
+    deviceSelector.option('~ choose output ~');
+    deviceSelector.disable('~ choose output ~');
+    deviceSelector.changed(midiOutputChange);
+
+    for (i=0;i<WebMidi.outputs.length;i++){
+      var name = WebMidi.outputs[i]._midiOutput.name;
+      devices[i] = "" + i + " " + name;
+      deviceSelector.option(devices[i]);
+    }
+    outputDevice = 0;
+
+    channelLabel = createP("channel:");
+    channelLabel.parent(midiParent);
+
+    channelSelector = createSelect();
+    channelSelector.parent(midiParent);
+    // channelSelector.addClass("select");
+    
+    for (j=0;j<8;j++){
+      channelSelector.option(j+1);
+    }
+
+    channelSelector.option("rows --> channels");
+    channelSelector.changed(midiChannelChange);
+    activeChannel = 1;
+    clockCount = 0;
+
+    clockCheckbox = createCheckbox(' send clock', false);
+    clockCheckbox.parent(midiParent);
+    clockCheckbox.class("checkbox");
+  }
+
+  
+
 
   context.suspend();
   touchIsDown = false;
@@ -131,9 +199,13 @@ function draw() {
   if (frameCount%speed == 0){
     if (!paused){
       generate();
+      clockCount = 0;
     }
   }
 
+  sendMidiClock();
+
+  
   for ( let i = 0; i < columns;i++) {
     for ( let j = 0; j < rows;j++) {
       var r = w/2;
@@ -141,7 +213,11 @@ function draw() {
       var y = j*w+r;
       
       if (mouseX > x-r && mouseX < x+r && mouseY > y-r && mouseY < y+r && board[i][j] != 1) {
-        ellipse(i * w + w/2, j * w + w/2, w/2);
+        push();
+          noFill();
+          stroke(255);
+          ellipse(i * w + w/2, j * w + w/2, w/2);
+        pop();
         if (mouseIsPressed){
           if (board[i][j] == 0){
             board[i][j] = 1;
@@ -150,25 +226,30 @@ function draw() {
         }
       }
       else if (touchIsDown){
-          if (touches[0].x > x-r && touches[0].x < x+r && touches[0].y > y-r && touches[0].y < y+r && board[i][j] != 1) {
-          board[i][j] = 1;
-          playVoice(i, j);
+        if (touches[0] != null){
+            if (touches[0].x > x-r && touches[0].x < x+r && touches[0].y > y-r && touches[0].y < y+r && board[i][j] != 1) {
+            board[i][j] = 1;
+            playVoice(i, j);
+          }
         }
       }
-      else if ((board[i][j] == 1)) fill(255);
+      
+      if ((board[i][j] == 1)) fill(255);
       else noFill();
-      stroke(255);
-      ellipse(i * w + w/2, j * w + w/2, w);
-
+      push();
+        stroke(255);
+        ellipse(i * w + w/2, j * w + w/2, w);
+      pop();
 
     }
   }
 
 }
 
-// function mousePressed() {
-//   init();
-// }
+function mousePressed() {
+  userStartAudio();
+  getAudioContext().resume();
+}
 
 // Fill board randomly
 function randomize() {
@@ -191,11 +272,38 @@ function randomize() {
       if (board[i][j] == 1) playVoice(i, j);
     }
   }
+
+  if (clockCheckbox.checked()){
+    WebMidi.outputs[outputDevice].sendStart();
+  }
+}
+
+// clear the board
+function clearButton() {
+  frameCount = 0;
+
+  for (let i = 0; i < columns; i++) {
+    for (let j = 0; j < rows; j++) {
+      board[i][j] = 0;
+      next[i][j] = 0;
+    }
+  }
+
+  for (let x = 0; x<rows;x++){
+    for (let y = 0; y<columns, y++;){
+      synths[x][y].releaseVoice();
+    }
+  }
+
+  if (clockCheckbox.checked()){
+    WebMidi.outputs[outputDevice].sendStop();
+  }
 }
 
 // The process of creating the new generation
 function generate() {
 
+  
   // Loop through every spot in our 2D array and check spots neighbors
   for (let x = 0; x < columns; x++) {
     for (let y = 0; y < rows; y++) {
@@ -251,21 +359,35 @@ function generate() {
 
 function playVoice(x, y){
   let note = notes[x];
-  let vel = map(y, 0, rows, 0.01, 0.5);
+
   envelopes[x][y].triggerAttack();
   // synths[x][y].amp(0.3, 0.0001);
+
+  if (webMidiSupported && scaleVal != 0){
+    if (activeChannel != "r"){
+      WebMidi.outputs[outputDevice].playNote(notes[x+(rows-y-1)], activeChannel);
+    } else {
+      WebMidi.outputs[2].playNote(notes[x+(rows-y-1)], rows-y);
+    }
+  }
 }
 
 function releaseVoice(x, y){
   let note = notes[x];
+
   envelopes[x][y].triggerRelease();
   // synths[x][y].amp(0, 0.0001);
+  if (webMidiSupported && scaleVal != 0){
+    if (activeChannel != "r"){
+      WebMidi.outputs[outputDevice].stopNote(notes[x+(rows-y-1)], activeChannel);
+    } else {
+      WebMidi.outputs[outputDevice].stopNote(notes[x+(rows-y-1)], rows-y);
+    }
+  }
 }
 
 function speedSliderChange(){
-  speed = floor(speedSlider.value);
-  // console.log(speed);
-  // speedP.innerHTML = "speed: " + speed;
+  speed = floor(100-speedSlider.value);
 }
 
 function volumeSliderChange(){
@@ -274,10 +396,14 @@ function volumeSliderChange(){
 }
 
 function octUp(){
-  for (i=0;i<notes.length;i++){
-    if (scaleVal != 0){
-      notes[i] += 12;
-    } else {
+  if (scaleVal != 0){
+    if (notes[notes.length-1] < 127){
+      for (i=0;i<notes.length;i++){
+        notes[i] += 12;
+      }
+    }
+  } else {
+    for (i=0;i<notes.length;i++){
       notes[i] = notes[i]*2;
     }
   }
@@ -288,7 +414,9 @@ function octDown(){
   
   for (i=0;i<notes.length;i++){
     if (scaleVal != 0){
-      notes[i] -= 12;
+      if (notes[0] > 0){
+        notes[i] -= 12;
+      }
     } else {
       notes[i] = notes[i]*0.5;
     }
@@ -331,6 +459,15 @@ function filterSliderChange(){
 }
 
 function remapNotes(){
+  for (i=0;i<notes.length;i++){
+    while (notes[i] > 127){
+      notes[i] -= 12;
+    }
+    
+    while (notes[i] < 0){
+      notes[i] += 12;
+    }
+  }
   if (scaleVal != 0){
     for (i=0;i<columns;i++){
       for (j=0;j<rows;j++){
@@ -358,6 +495,8 @@ function windowResized(){
 
 function touchStarted(){
   touchIsDown = true;
+  userStartAudio();
+  getAudioContext().resume();
 }
 function touchEnded(){
   touchIsDown = false;
@@ -383,6 +522,9 @@ function playButton(){
       if (board[i][j] == 1) playVoice(i, j);
     }
   }
+  if (clockCheckbox.checked()){
+    WebMidi.outputs[outputDevice].sendStart();
+  }
 
 }
 
@@ -400,24 +542,68 @@ function scaleSelector(scaleRadio){
    makeMicroNotes();
       
   } else if (scaleVal == 1){
+    // CHROMATIC
     notes = [48, 49, 50, 51, 52, 53, 54, 
             55, 56, 57 ,58 ,59 ,60, 61,
             62, 63, 64, 65, 66, 67, 68,
             69, 70, 71];
 
   } else if (scaleVal == 2){
+    // DIATONIC
     notes = [48, 50, 52, 53, 55, 57, 59, 
       60, 62, 64, 65, 67, 69, 71, 
       72, 74, 76, 77, 79, 81, 83, 
       84, 86, 88];
   } else if (scaleVal == 3){
+    // WHOLETONE
     notes[0] = 48;
-    for (i=1;i<notes.length;i++){
-      notes[i] = notes[i-1] + 2;
+    for (i=1;i<25;i++){
+      notes[i] = (notes[i-1]) + 2;
+    }
+  } else if (scaleVal == 4){
+    // PENTATONIC
+    notes = [36, 38, 40, 43, 45];
+    for (i=5;i<25;i++){
+      notes[i] = (notes[i-5]) + 12;
+    }
+  } else if (scaleVal == 5){
+    // MAJ 7
+    notes = [36, 38, 40, 43, 47];
+    for (i=5;i<25;i++){
+      notes[i] = (notes[i-5]) + 12;
+    }
+  } else if (scaleVal == 6){
+    // MIN 7
+    notes = [36, 39, 43, 46];
+    for (i=4;i<25;i++){
+      notes[i] = (notes[i-4]) + 12;
+    }
+  } else if (scaleVal == 7){
+    // FOURTHS
+    notes[0] = 36;
+    for (i=1;i<25;i++){
+      notes[i] = (notes[i-1]) + 5;
+    }
+  }  else if (scaleVal == 8){
+    // FIFTHS
+    notes[0] = 12;
+    for (i=1;i<25;i++){
+      notes[i] = (notes[i-1]) + 7;
     }
   }
 
   remapNotes();
+
+}
+
+function oscSelector(newOsc){
+  var newOsc = newOsc.value;
+
+  for (i=0;i<columns;i++){
+    for (j=0;j<rows;j++){
+      synths[i][j].setType(newOsc);
+    }
+  }
 
 }
 
@@ -429,3 +615,38 @@ function makeMicroNotes(){
   }
 }
 
+function midiOutputChange(){
+  var newDevice = deviceSelector.value().charAt(0);
+  console.log("midi output device changed to: " + newDevice);
+  outputDevice = newDevice;
+}
+
+function midiChannelChange(){
+  WebMidi.outputs[outputDevice].sendStop();
+  var newChannel = channelSelector.value().charAt(0);
+  console.log("output channel changed to: " + channelSelector.value());
+  activeChannel = newChannel;
+}
+
+function sendMidiClock(){
+  // send midi clock
+  if (webMidiSupported){
+    if (clockCheckbox.checked()){
+      if (speed >= 24){
+        if (floor(clockCount%(speed/24)) == 0){
+          if (!paused && webMidiSupported){
+            WebMidi.outputs[outputDevice].sendClock();
+          }
+        }
+      } else {
+        if (floor(clockCount%(speed/(speed*0.5)) == 0)){
+          if (!paused && webMidiSupported){
+            WebMidi.outputs[outputDevice].sendClock();
+          }
+        }
+      }
+    }
+  }
+  clockCount++;
+
+}
